c6f6f8f78c2085771c83b93d6a13966c58b57645
Stonesusu <Stonesusu@163.com>
2024-05-20 14:04:48 +0800

    commit all

--- train.sh
+++ train.sh
@@ -32,7 +32,7 @@ for i in "${!depth[@]}";do
         save_dir=./checkpoints/${base_dir}/
         CUDA_VISIBLE_DEVICES=${device} python train.py \
         --dataset mvtec \
-        --train_data_path /remote-home/iot_zhouqihang/data/mvdataset \
+        --train_data_path /mnt/workspace/mvtec_anomaly_detection \
         --save_path ${save_dir} \
         --features_list 6 12 18 24 --image_size 518  --batch_size 8 --print_freq 1 \
         --epoch 100 \
